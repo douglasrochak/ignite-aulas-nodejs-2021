1280e7cdd3b1d27799acd84ee921476436dcc4c9
meamedemontao <douglas.srochak@gmail.com>
2023-02-22 19:27:17 -0300

    rentalx criando use case de especificações

--- 02-rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ 02-rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,4 +1,4 @@
-import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository'
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository'
 
 interface IRequest {
   name: string
@@ -11,7 +11,6 @@ class CreateSpecificationService {
 
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists = this.specificationRepository.findByName(name)
-    console.log('specificationAlreadyExists', specificationAlreadyExists)
 
     if (specificationAlreadyExists) {
       throw new Error('Category already exists!')
--- 02-rentalx/src/routes/specifications.routes.ts
+++ 02-rentalx/src/routes/specifications.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express'
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase'
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
 const specificationRoutes = Router()
 
-const specificationsRepository = new SpecificationsRepository()
-
 specificationRoutes.post('/', (request, response) => {
-  const { name, description } = request.body
-  const createSpecificationService = new CreateCategoryUseCase(specificationsRepository)
-
-  createSpecificationService.execute({ name, description })
-
-  return response.status(201).json()
+  createSpecificationController.handle(request, response)
 })
 
 export { specificationRoutes }
